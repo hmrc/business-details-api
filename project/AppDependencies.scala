087f64ece284dd4bf54c5f0435bc7c2fba08e13b
Adrian Jones <14924378+Nboaram@users.noreply.github.com>
2020-03-27 08:56:44 +0000

    Added Template (#1)
    * ADDED TEMPLATE
    
    * Port and api name
    
    * Port
    
    * application.conf
    
    Co-authored-by: Adrian Jones <nboaram@users.noreply.github.com>

---
+++ project/AppDependencies.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import play.core.PlayVersion
+import play.sbt.PlayImport._
+import sbt._
+
+object AppDependencies {
+
+  val compile: Seq[ModuleID] = Seq(
+    ws,
+    "uk.gov.hmrc"   %% "bootstrap-play-26" % "1.5.0",
+    "uk.gov.hmrc"   %% "domain"            % "5.6.0-play-26",
+    "uk.gov.hmrc"   %% "play-hmrc-api"     % "4.1.0-play-26",
+    "org.typelevel" %% "cats-core"         % "2.1.0",
+    compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.4.4" cross CrossVersion.full),
+    "com.github.ghik" % "silencer-lib" % "1.4.4" % Provided cross CrossVersion.full,
+    "com.chuusai"   %% "shapeless"         % "2.3.3"
+  )
+
+  def test(scope: String = "test, it"): Seq[sbt.ModuleID] = Seq(
+    "org.scalatest"          %% "scalatest"          % "3.1.0"             % scope,
+    "com.vladsch.flexmark"   % "flexmark-all"        % "0.35.10"           % scope,
+    "org.scalacheck"         %% "scalacheck"         % "1.14.2"            % scope,
+    "org.scalamock"          %% "scalamock"          % "4.4.0"             % scope,
+    "com.typesafe.play"      %% "play-test"          % PlayVersion.current % scope,
+    "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2"             % scope,
+    "com.github.tomakehurst" % "wiremock"            % "2.25.1"            % scope
+  )
+
+  // Fixes a transitive dependency clash between wiremock and scalatestplus-play
+  val overrides: Seq[ModuleID] = {
+    val jettyFromWiremockVersion = "9.2.24.v20180105"
+    Seq(
+      "org.eclipse.jetty"           % "jetty-client"       % jettyFromWiremockVersion,
+      "org.eclipse.jetty"           % "jetty-continuation" % jettyFromWiremockVersion,
+      "org.eclipse.jetty"           % "jetty-http"         % jettyFromWiremockVersion,
+      "org.eclipse.jetty"           % "jetty-io"           % jettyFromWiremockVersion,
+      "org.eclipse.jetty"           % "jetty-security"     % jettyFromWiremockVersion,
+      "org.eclipse.jetty"           % "jetty-server"       % jettyFromWiremockVersion,
+      "org.eclipse.jetty"           % "jetty-servlet"      % jettyFromWiremockVersion,
+      "org.eclipse.jetty"           % "jetty-servlets"     % jettyFromWiremockVersion,
+      "org.eclipse.jetty"           % "jetty-util"         % jettyFromWiremockVersion,
+      "org.eclipse.jetty"           % "jetty-webapp"       % jettyFromWiremockVersion,
+      "org.eclipse.jetty"           % "jetty-xml"          % jettyFromWiremockVersion,
+      "org.eclipse.jetty.websocket" % "websocket-api"      % jettyFromWiremockVersion,
+      "org.eclipse.jetty.websocket" % "websocket-client"   % jettyFromWiremockVersion,
+      "org.eclipse.jetty.websocket" % "websocket-common"   % jettyFromWiremockVersion
+    )
+  }
+}
