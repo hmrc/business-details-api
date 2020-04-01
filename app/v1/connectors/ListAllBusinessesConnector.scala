3ca6d67298245c2139cc863f0f1155ee4af00955
“Thepworth95” <56401451+Thepworth95@users.noreply.github.com>
2020-04-01 10:33:26 +0100

    [MTDSA-5124] - List all businesses Connector

---
+++ app/v1/connectors/ListAllBusinessesConnector.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.connectors
+
+import config.AppConfig
+import javax.inject.Inject
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.bootstrap.http.HttpClient
+import v1.models.request.listAllBusinesses.ListAllBusinessesRequest
+import v1.models.response.listAllBusiness.ListResponse
+import v1.connectors.httpparsers.StandardDesHttpParser._
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class ListAllBusinessesConnector @Inject()(val http: HttpClient,
+                                           val appConfig: AppConfig) extends BaseDesConnector {
+  def listAllBusinesses(request: ListAllBusinessesRequest)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[DesOutcome[ListResponse]] = {
+
+    val url = s"registration/business-details/nino/${request.nino}"
+    get(
+      DesUri[ListResponse](s"$url")
+    )
+  }
+}
