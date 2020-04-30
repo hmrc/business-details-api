5022ad152999a6f2b3d431b1f1f8fc318459920a
James Whiteley <31282758+james-work-account@users.noreply.github.com>
2020-04-30 09:42:23 +0100

    modified definition tests to pass

--- app/definition/ApiDefinitionFactory.scala
+++ app/definition/ApiDefinitionFactory.scala
@@ -43,7 +43,7 @@ class ApiDefinitionFactory @Inject()(appConfig: AppConfig) {
       ),
       api = APIDefinition(
         name = "Business Details (MTD)",
-        description = "An API for ...",
+        description = "An API for providing business details data",
         context = appConfig.apiGatewayContext,
         categories = Seq("INCOME_TAX_MTD"),
         versions = Seq(
