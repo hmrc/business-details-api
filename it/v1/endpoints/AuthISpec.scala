c9cfc04cbff92cdae6354d75e7325d0bc2207e96
“DreamcastKnight” <56401347+DreamcastKnight@users.noreply.github.com>
2020-05-06 09:29:43 +0100

    [MTDSA-5381] - removed sample endpoint from raml & increased coverage

--- it/v1/endpoints/AuthISpec.scala
+++ it/v1/endpoints/AuthISpec.scala
@@ -28,16 +28,6 @@ class AuthISpec extends IntegrationBaseSpec {
 
   private trait Test {
     val nino          = "AA123456A"
-    val taxYear       = "2017-18"
-    val data        = "someData"
-    val correlationId = "X-123"
-
-    val requestJson: String =
-      s"""
-         |{
-         |"data": "$data"
-         |}
-    """.stripMargin
 
     def setupStubs(): StubMapping
 
