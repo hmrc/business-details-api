c789810fca1914c24f01bcfe42719f4a55a8c028
rhysconnor <32180830+rhysconnor@users.noreply.github.com>
2023-07-31 11:51:36 +0100

    add rule incorrect GTS error handling (#101)

--- app/api/models/errors/CommonMtdErrors.scala
+++ app/api/models/errors/CommonMtdErrors.scala
@@ -55,3 +55,6 @@ object ClientNotAuthenticatedError extends MtdError("CLIENT_OR_AGENT_NOT_AUTHORI
 object InvalidAcceptHeaderError extends MtdError("ACCEPT_HEADER_INVALID", "The accept header is missing or invalid", NOT_ACCEPTABLE)
 object UnsupportedVersionError  extends MtdError("NOT_FOUND", "The requested resource could not be found", NOT_FOUND)
 object InvalidBodyTypeError     extends MtdError("INVALID_BODY_TYPE", "Expecting text/json or application/json body", UNSUPPORTED_MEDIA_TYPE)
+
+//Stub Errors
+object RuleIncorrectGovTestScenarioError extends MtdError("RULE_INCORRECT_GOV_TEST_SCENARIO", "The Gov-Test-Scenario was not found", BAD_REQUEST)
