51a538ed36026c026bd019d8be7384201b8bc80a
KobithasanVasantharajah <110105165+KobithasanVasantharajah@users.noreply.github.com>
2023-09-28 17:01:47 +0100

    MTDSA-19434: OasFeatureRewriter - business-details-api

--- test/config/rewriters/OasFeatureRewriterSpec.scala
+++ test/config/rewriters/OasFeatureRewriterSpec.scala
@@ -24,79 +24,112 @@ import support.UnitSpec
 
 class OasFeatureRewriterSpec extends UnitSpec with MockAppConfig {
 
-  private def setupCheckAndRewrite(oasFeatureEnabled: Boolean, oasFeatureEnabledInProd: Boolean): (CheckRewrite, Rewriter) = {
+  private def setupCheckAndRewrite(oasFeatureEnabled: Boolean, oasFeatureReleasedInProd: Boolean): (CheckRewrite, Rewriter) = {
     MockAppConfig.featureSwitches returns Configuration(
       "oasFeature.enabled"                -> oasFeatureEnabled,
-      "oasFeature.released-in-production" -> oasFeatureEnabledInProd
+      "oasFeature.released-in-production" -> oasFeatureReleasedInProd
     )
 
     val rewriter = new OasFeatureRewriter()(mockAppConfig)
     rewriter.rewriteOasFeature.asTuple
   }
 
-  "check and rewrite" should {
-    "indicate that it wants to rewrite the file" in {
-      val (check, _) = setupCheckAndRewrite(oasFeatureEnabled = true, oasFeatureEnabledInProd = true)
-      val result     = check("1.0", "oasFeature")
-      result shouldBe true
+  "check" should {
+    "always return true" in {
+      List(
+        (true, true),
+        (true, false),
+        (false, true),
+        (false, false)
+      ).foreach { case (enabled, enabledInProd) =>
+        val (check, _) = setupCheckAndRewrite(oasFeatureEnabled = enabled, oasFeatureReleasedInProd = enabledInProd)
+
+        val result = check("1.0", "any-file.yaml")
+        result shouldBe true
+      }
+    }
+  }
+
+  "rewrite" should {
+
+    val yaml =
+      """
+        |summary: Retrieve Employment Expenses
+        |description: |
+        |  This endpoint enables you to retrieve existing employment expenses.
+        |  A National Insurance number and tax year must be provided.
+        |
+        |  ### Test data
+        |  {{#if (enabled "oasFeature")}}
+        |  <p>{{#unless (releasedInProduction "oasFeature")}}[Test Only] {{/unless}}Scenario simulations using Gov-Test-Scenario headers are only available in the sandbox environment.</p>
+        |  {{/if}}
+        |
+        |tags:
+        |  - Employment Expenses
+        |""".stripMargin
+
+    "show the field without [Test Only]" when {
+      "the feature is enabled in environment and in prod" in {
+        val (_, rewrite) = setupCheckAndRewrite(oasFeatureEnabled = true, oasFeatureReleasedInProd = true)
+
+        val expected =
+          s"""
+             |summary: Retrieve Employment Expenses
+             |description: |
+             |  This endpoint enables you to retrieve existing employment expenses.
+             |  A National Insurance number and tax year must be provided.
+             |
+             |  ### Test data
+             |${" "}${" "}
+             |  <p>Scenario simulations using Gov-Test-Scenario headers are only available in the sandbox environment.</p>
+             |${" "}${" "}
+             |
+             |tags:
+             |  - Employment Expenses
+             |""".stripMargin
+
+        val result = rewrite("/...", "something.yaml", yaml)
+        result shouldBe expected
+      }
     }
 
-    "rewrite" when {
-
-      val yaml =
-        """
-          |summary: Retrieve Business Details
-          |description: |
-          |  This endpoint enables you to retrieve existing business details.
-          |  A National Insurance number and tax year must be provided.
-          |
-          |  ### Test data
-          |  {{#if (testOnly "oasFeature")}}
-          |  <p>Scenario simulations using Gov-Test-Scenario headers ARE ONLY AVAILABLE IN the sandbox environment.</p>
-          |  {{else}}
-          |  <p>Scenario simulations using Gov-Test-Scenario headers are only available in the sandbox environment.</p>
-          |  {{/if}}
-          |
-          |tags:
-          |  - Employment Expenses
-          |""".stripMargin
-
-      "the feature isn't enabled" in {
-        val (_, rewrite) = setupCheckAndRewrite(oasFeatureEnabled = false, oasFeatureEnabledInProd = false)
-
-        val expected =
-          s"""
-             |summary: Retrieve Business Details
-             |description: |
-             |  This endpoint enables you to retrieve existing business details.
-             |  A National Insurance number and tax year must be provided.
-             |
-             |  ### Test data
-             |${" "}${" "}
-             |  <p>Scenario simulations using Gov-Test-Scenario headers are only available in the sandbox environment.</p>
-             |${" "}${" "}
-             |
-             |tags:
-             |  - Employment Expenses
-             |""".stripMargin
-
-        val result = rewrite("/...", "something.yaml", yaml)
-        result shouldBe expected
-      }
-
+    "show the field with [Test Only]" when {
       "the feature is enabled in environment but not in prod" in {
-        val (_, rewrite) = setupCheckAndRewrite(oasFeatureEnabled = true, oasFeatureEnabledInProd = false)
+        val (_, rewrite) = setupCheckAndRewrite(oasFeatureEnabled = true, oasFeatureReleasedInProd = false)
 
         val expected =
           s"""
-             |summary: Retrieve Business Details
+             |summary: Retrieve Employment Expenses
              |description: |
-             |  This endpoint enables you to retrieve existing business details.
+             |  This endpoint enables you to retrieve existing employment expenses.
              |  A National Insurance number and tax year must be provided.
              |
              |  ### Test data
              |${" "}${" "}
-             |  <p>Scenario simulations using Gov-Test-Scenario headers ARE ONLY AVAILABLE IN the sandbox environment.</p>
+             |  <p>[Test Only] Scenario simulations using Gov-Test-Scenario headers are only available in the sandbox environment.</p>
+             |${" "}${" "}
+             |
+             |tags:
+             |  - Employment Expenses
+             |""".stripMargin
+
+        val result = rewrite("/...", "something.yaml", yaml)
+        result shouldBe expected
+      }
+    }
+
+    "not show the field" when {
+      "the feature is disabled in environment but enabled in prod" in {
+        val (_, rewrite) = setupCheckAndRewrite(oasFeatureEnabled = false, oasFeatureReleasedInProd = true)
+
+        val expected =
+          s"""
+             |summary: Retrieve Employment Expenses
+             |description: |
+             |  This endpoint enables you to retrieve existing employment expenses.
+             |  A National Insurance number and tax year must be provided.
+             |
+             |  ### Test data
              |${" "}${" "}
              |
              |tags:
@@ -107,19 +140,17 @@ class OasFeatureRewriterSpec extends UnitSpec with MockAppConfig {
         result shouldBe expected
       }
 
-      "the feature is enabled in environment and in prod" in {
-        val (_, rewrite) = setupCheckAndRewrite(oasFeatureEnabled = true, oasFeatureEnabledInProd = true)
+      "the feature isn't enabled in environment and prod" in {
+        val (_, rewrite) = setupCheckAndRewrite(oasFeatureEnabled = false, oasFeatureReleasedInProd = false)
 
         val expected =
           s"""
-             |summary: Retrieve Business Details
+             |summary: Retrieve Employment Expenses
              |description: |
-             |  This endpoint enables you to retrieve existing business details.
+             |  This endpoint enables you to retrieve existing employment expenses.
              |  A National Insurance number and tax year must be provided.
              |
              |  ### Test data
-             |${" "}${" "}
-             |  <p>Scenario simulations using Gov-Test-Scenario headers are only available in the sandbox environment.</p>
              |${" "}${" "}
              |
              |tags:
