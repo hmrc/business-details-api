1cbfd1af31877ec688051c28cfd9f9596dcd969d
Thepworth95 <56401451+Thepworth95@users.noreply.github.com>
2025-06-03 11:44:33 +0100

    retrieve accounting type connector and service

---
+++ test/v2/retrieveAccountingType/RetrieveAccountingTypeConnectorSpec.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.retrieveAccountingType
+
+import api.connectors.{ConnectorSpec, DownstreamOutcome}
+import api.models.domain.{BusinessId, Nino, TaxYear}
+import api.models.errors.{DownstreamErrorCode, DownstreamErrors}
+import api.models.outcomes.ResponseWrapper
+import v2.common.models.AccountingType
+import v2.retrieveAccountingType.model.request._
+import v2.retrieveAccountingType.model.response.RetrieveAccountingTypeResponse
+
+import scala.concurrent.Future
+
+class RetrieveAccountingTypeConnectorSpec extends ConnectorSpec {
+
+  private val nino       = Nino("AA123456A")
+  private val businessId = BusinessId("XAIS12345678910")
+  private val taxYear    = TaxYear.fromMtd("2024-25")
+
+  private val request  = RetrieveAccountingTypeRequest(nino, businessId, taxYear)
+  private val response = RetrieveAccountingTypeResponse(AccountingType.CASH)
+
+  val queryParams = Map(
+    "incomeSourceId"  -> "XAIS12345678910",
+    "taxYearExplicit" -> "2025"
+  )
+
+  val mappedQueryParams: Map[String, String] = queryParams.collect { case (k: String, v: String) => (k, v) }
+
+  "retrieveAccountingTypeConnector" must {
+    "return a successful response" when {
+      "the downstream request is successful" in new HipTest with Test {
+        val outcome: Right[Nothing, ResponseWrapper[RetrieveAccountingTypeResponse]] = Right(ResponseWrapper(correlationId, response))
+
+        willGet(s"$baseUrl/itsd/income-sources/v2/$nino", mappedQueryParams.toList).returns(Future.successful(outcome))
+
+        val result: DownstreamOutcome[RetrieveAccountingTypeResponse] = await(connector.retrieveAccountingType(request))
+        result shouldBe outcome
+      }
+    }
+
+    "return an unsuccessful response" when {
+      "the downstream request is unsuccessful" in new HipTest with Test {
+        val downstreamErrorResponse: DownstreamErrors                 = DownstreamErrors.single(DownstreamErrorCode("SOME_ERROR"))
+        val outcome: Left[ResponseWrapper[DownstreamErrors], Nothing] = Left(ResponseWrapper(correlationId, downstreamErrorResponse))
+
+        willGet(s"$baseUrl/itsd/income-sources/v2/$nino", mappedQueryParams.toList).returns(Future.successful(outcome))
+
+        val result: DownstreamOutcome[RetrieveAccountingTypeResponse] = await(connector.retrieveAccountingType(request))
+        result shouldBe outcome
+      }
+    }
+  }
+
+  trait Test { _: ConnectorTest =>
+    protected val connector: RetrieveAccountingTypeConnector = new RetrieveAccountingTypeConnector(mockHttpClient, mockAppConfig)
+  }
+
+}
---
+++ test/v2/retrieveAccountingType/RetrieveAccountingTypeServiceSpec.scala
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.retrieveAccountingType
+
+import api.models.domain.{BusinessId, Nino, TaxYear}
+import api.models.errors._
+import api.models.outcomes.ResponseWrapper
+import api.services.{ServiceOutcome, ServiceSpec}
+import v2.common.models.AccountingType
+import v2.retrieveAccountingType.model.request._
+import v2.retrieveAccountingType.model.response.RetrieveAccountingTypeResponse
+
+import scala.concurrent.Future
+
+class RetrieveAccountingTypeServiceSpec extends ServiceSpec {
+
+  private val nino             = Nino("AA123456A")
+  private val businessId       = BusinessId("XAIS12345678910")
+  private val taxYear          = TaxYear.fromMtd("2024-25")
+  private val requestData      = RetrieveAccountingTypeRequest(nino, businessId, taxYear)
+  private val expectedResponse = RetrieveAccountingTypeResponse(AccountingType.CASH)
+
+  "service" when {
+    "a connector call is successful" should {
+      "return a mapped result" in new Test {
+        MockedRetrieveAccountingTypeConnector
+          .retrieveAccountingType(requestData)
+          .returns(Future.successful(Right(ResponseWrapper(correlationId, expectedResponse))))
+
+        val result: ServiceOutcome[RetrieveAccountingTypeResponse] = await(service.retrieveAccountingType(requestData))
+        result shouldBe Right(ResponseWrapper(correlationId, expectedResponse))
+      }
+    }
+    "a connector call is unsuccessful" should {
+      def serviceError(downstreamErrorCode: String, error: MtdError): Unit =
+        s"return ${error.code} when $downstreamErrorCode error is returned from the service" in new Test {
+          MockedRetrieveAccountingTypeConnector
+            .retrieveAccountingType(requestData)
+            .returns(Future.successful(Left(ResponseWrapper(correlationId, DownstreamErrors.single(DownstreamErrorCode(downstreamErrorCode))))))
+
+          val result: ServiceOutcome[RetrieveAccountingTypeResponse] = await(service.retrieveAccountingType(requestData))
+          result shouldBe Left(ErrorWrapper(correlationId, error))
+        }
+
+      val errors = List(
+        ("1215", NinoFormatError),
+        ("1117", TaxYearFormatError),
+        ("1007", BusinessIdFormatError),
+        ("1122", InternalError),
+        ("1229", InternalError),
+        ("5009", InternalError),
+        ("5010", NotFoundError),
+        ("UNMATCHED_STUB_ERROR", RuleIncorrectGovTestScenarioError)
+      )
+
+      errors.foreach((serviceError _).tupled)
+    }
+  }
+
+  private trait Test extends MockRetrieveAccountingTypeConnector {
+
+    protected val service = new RetrieveAccountingTypeService(mockRetrieveAccountingTypeConnector)
+
+  }
+
+}
