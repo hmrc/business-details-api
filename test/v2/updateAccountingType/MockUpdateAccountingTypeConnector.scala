135851ce7bee6875b3b821cad9ce907b02a8023e
Thepworth95 <56401451+Thepworth95@users.noreply.github.com>
2025-06-04 15:16:04 +0100

    [MTDSA-30175] - update accounting type connector and service (#175)

---
+++ test/v2/updateAccountingType/MockUpdateAccountingTypeConnector.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.updateAccountingType
+
+import api.connectors.DownstreamOutcome
+import org.scalamock.handlers.CallHandler
+import org.scalamock.scalatest.MockFactory
+import uk.gov.hmrc.http.HeaderCarrier
+import v2.updateAccountingType.model.request.UpdateAccountingTypeRequestData
+
+import scala.concurrent.{ExecutionContext, Future}
+
+trait MockUpdateAccountingTypeConnector extends MockFactory {
+
+  val mockUpdateAccountingTypeConnector: UpdateAccountingTypeConnector = mock[UpdateAccountingTypeConnector]
+
+  object MockedUpdateAccountingTypeConnector {
+
+    def create(requestData: UpdateAccountingTypeRequestData): CallHandler[Future[DownstreamOutcome[Unit]]] = {
+      (
+        mockUpdateAccountingTypeConnector
+          .update(_: UpdateAccountingTypeRequestData)(
+            _: HeaderCarrier,
+            _: ExecutionContext,
+            _: String
+          )
+        )
+        .expects(requestData, *, *, *)
+    }
+
+  }
+
+}
---
+++ test/v2/updateAccountingType/UpdateAccountingTypeConnectorSpec.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.updateAccountingType
+
+import api.connectors.{ConnectorSpec, DownstreamOutcome}
+import api.models.domain.{BusinessId, Nino, TaxYear}
+import api.models.errors.{DownstreamErrorCode, DownstreamErrors}
+import api.models.outcomes.ResponseWrapper
+import v2.common.models.AccountingType
+import v2.updateAccountingType.model.request._
+
+import scala.concurrent.Future
+
+class UpdateAccountingTypeConnectorSpec extends ConnectorSpec {
+
+  private val nino       = Nino("AA123456A")
+  private val businessId = BusinessId("XAIS12345678910")
+  private val taxYear    = TaxYear.fromMtd("2024-25")
+  private val body       = UpdateAccountingTypeRequestBody(AccountingType.`CASH`)
+
+  private val request = UpdateAccountingTypeRequestData(nino, businessId, taxYear, body)
+
+  "UpdateAccountingTypeConnector" must {
+    "return a successful response" when {
+      "the downstream request is successful" in new HipTest with Test {
+        val outcome: Right[Nothing, ResponseWrapper[Unit]] = Right(ResponseWrapper(correlationId, ()))
+
+        willPut(s"$baseUrl/itsd/income-sources/$nino/accounting-type/$businessId?taxYear=${taxYear.asTysDownstream}", body)
+          .returns(Future.successful(outcome))
+
+        val result: DownstreamOutcome[Unit] = await(connector.update(request))
+        result shouldBe outcome
+      }
+    }
+
+    "return an unsuccessful response" when {
+      "the downstream request is unsuccessful" in new HipTest with Test {
+        val downstreamErrorResponse: DownstreamErrors                 = DownstreamErrors.single(DownstreamErrorCode("SOME_ERROR"))
+        val outcome: Left[ResponseWrapper[DownstreamErrors], Nothing] = Left(ResponseWrapper(correlationId, downstreamErrorResponse))
+
+        willPut(s"$baseUrl/itsd/income-sources/$nino/accounting-type/$businessId?taxYear=${taxYear.asTysDownstream}", body)
+          .returns(Future.successful(outcome))
+
+        val result: DownstreamOutcome[Unit] = await(connector.update(request))
+        result shouldBe outcome
+      }
+    }
+  }
+
+  trait Test { _: ConnectorTest =>
+    protected val connector: UpdateAccountingTypeConnector = new UpdateAccountingTypeConnector(mockHttpClient, mockAppConfig)
+  }
+
+}
