85e480c67d9947d11112b5d38f646a1a332f0933
Ben Ryan <37295698+benryan1790@users.noreply.github.com>
2025-06-11 20:49:49 +0100

    MTDSA-30188: connector and service (#184)

---
+++ test/v2/createUpdatePeriodsOfAccount/CreateUpdatePeriodsOfAccountConnectorSpec.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.createUpdatePeriodsOfAccount
+
+import api.connectors.ConnectorSpec
+import api.models.domain.{BusinessId, Nino, TaxYear}
+import api.models.errors.{DownstreamErrorCode, DownstreamErrors}
+import api.models.outcomes.ResponseWrapper
+import v2.common.models.PeriodsOfAccountDates
+import v2.createUpdatePeriodsOfAccount.request.{CreateUpdatePeriodsOfAccountRequestBody, CreateUpdatePeriodsOfAccountRequestData}
+
+import scala.concurrent.Future
+
+class CreateUpdatePeriodsOfAccountConnectorSpec extends ConnectorSpec {
+
+  private val nino       = Nino("AA123456A")
+  private val businessId = BusinessId("XAIS12345678910")
+  private val taxYear    = TaxYear.fromMtd("2024-25")
+  private val body       = CreateUpdatePeriodsOfAccountRequestBody(true, Some(Seq(PeriodsOfAccountDates("2024-04-06", "2025-04-05"))))
+  private val request    = CreateUpdatePeriodsOfAccountRequestData(nino, businessId, taxYear, body)
+
+  "CreateUpdatePeriodsOfAccountConnector" should {
+    "return a successful response" when {
+      "the downstream request is successful" in new HipTest with Test {
+        val outcome: Right[Nothing, ResponseWrapper[Unit]] = Right(ResponseWrapper(correlationId, ()))
+
+        willPut(downStreamUri, body)
+          .returns(Future.successful(outcome))
+
+        val result = await(connector.create(request))
+        result shouldBe outcome
+      }
+    }
+
+    "return an unsuccessful response" when {
+      "the downstream request is unsuccessful" in new HipTest with Test {
+        val downstreamErrorResponse: DownstreamErrors                 = DownstreamErrors.single(DownstreamErrorCode("SOME_ERROR"))
+        val outcome: Left[ResponseWrapper[DownstreamErrors], Nothing] = Left(ResponseWrapper(correlationId, downstreamErrorResponse))
+
+        willPut(downStreamUri, body)
+          .returns(Future.successful(outcome))
+
+        val result = await(connector.create(request))
+        result shouldBe outcome
+      }
+    }
+  }
+
+  trait Test { _: ConnectorTest =>
+    protected val downStreamUri = s"$baseUrl/itsd/income-sources/$nino/periods-of-account/$businessId?taxYear=${taxYear.asTysDownstream}"
+    protected val connector: CreateUpdatePeriodsOfAccountConnector = new CreateUpdatePeriodsOfAccountConnector(mockHttpClient, mockAppConfig)
+  }
+
+}
---
+++ test/v2/createUpdatePeriodsOfAccount/CreateUpdatePeriodsOfAccountServiceSpec.scala
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v2.createUpdatePeriodsOfAccount
+
+import api.models.domain.{BusinessId, Nino, TaxYear}
+import api.models.errors._
+import api.models.outcomes.ResponseWrapper
+import api.services.{ServiceOutcome, ServiceSpec}
+import v2.common.models.PeriodsOfAccountDates
+import v2.createUpdatePeriodsOfAccount.request.{CreateUpdatePeriodsOfAccountRequestBody, CreateUpdatePeriodsOfAccountRequestData}
+
+import scala.concurrent.Future
+
+class CreateUpdatePeriodsOfAccountServiceSpec extends ServiceSpec {
+
+  private val nino       = Nino("AA123456A")
+  private val businessId = BusinessId("XAIS12345678910")
+  private val taxYear    = TaxYear.fromMtd("2024-25")
+  private val body       = CreateUpdatePeriodsOfAccountRequestBody(true, Some(Seq(PeriodsOfAccountDates("2024-04-06", "2025-04-05"))))
+  private val request    = CreateUpdatePeriodsOfAccountRequestData(nino, businessId, taxYear, body)
+
+  "CreateUpdatePeriodsOfAccountService" when {
+    "the connector call is successful" should {
+      "return a mapped result" in new Test {
+
+        MockedCreateUpdatePeriodsOfAccountConnector
+          .create(request)
+          .returns(Future.successful(Right(ResponseWrapper(correlationId, ()))))
+
+        val result = await(service.create(request))
+        result shouldBe Right(ResponseWrapper(correlationId, ()))
+
+      }
+    }
+
+    "a connector call is unsuccessful" should {
+      def serviceError(downstreamErrorCode: String, error: MtdError): Unit =
+        s"return ${error.code} when $downstreamErrorCode error is returned from the service" in new Test {
+          MockedCreateUpdatePeriodsOfAccountConnector
+            .create(request)
+            .returns(Future.successful(Left(ResponseWrapper(correlationId, DownstreamErrors.single(DownstreamErrorCode(downstreamErrorCode))))))
+
+          val result: ServiceOutcome[Unit] = await(service.create(request))
+          result shouldBe Left(ErrorWrapper(correlationId, error))
+        }
+
+      val errors = List(
+        "1000"                 -> InternalError,
+        "1007"                 -> BusinessIdFormatError,
+        "1117"                 -> TaxYearFormatError,
+        "1128"                 -> RuleEndBeforeStartDateError,
+        "1129"                 -> RuleStartDateError,
+        "1130"                 -> RuleEndDateError,
+        "1131"                 -> RulePeriodsOverlapError,
+        "1132"                 -> RuleCessationDateError,
+        "1215"                 -> NinoFormatError,
+        "1216"                 -> InternalError,
+        "4200"                 -> RuleOutsideAmendmentWindowError,
+        "5000"                 -> RuleTaxYearNotSupportedError,
+        "5010"                 -> NotFoundError,
+        "UNMATCHED_STUB_ERROR" -> RuleIncorrectGovTestScenarioError
+      )
+
+      errors.foreach((serviceError _).tupled)
+    }
+  }
+
+  private trait Test extends MockCreateUpdatePeriodsOfAccountConnector {
+
+    protected val service = new CreateUpdatePeriodsOfAccountService(mockCreateUpdatePeriodsOfAccountConnector)
+  }
+
+}
